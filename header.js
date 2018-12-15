c67a6670d4cc3c9622b03c68a2d2bd12472ec481
sxb822 <43203733+sxb822@users.noreply.github.com>
2018-12-15 14:20:48 +0800

    Add files via upload

---
+++ header.js
@@ -0,0 +1,8 @@
+document.write("头部文档的功能<br>");
+document.write("头部文档的功能2<br>");
+module.exports = {
+    run: function () {
+        document.write("添加头部功能1<br>");
+        document.write("添加头部功能2<br>");
+    }
+}
