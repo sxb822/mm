0d15ecaaafde7f74bd9f62f5217bc30305302183
sxb822 <43203733+sxb822@users.noreply.github.com>
2018-12-15 14:47:36 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+//导入css模块
+require("!style-loader!css-loader!../style/index.css");
+//导入第三方 模块
+//require("./header.js");
+var obj = require("./header.js");
+obj.run();
+document.write("阿法士大夫");
+document.write("阿法士大夫");
+document.write("阿法士大夫<br>");
+document.write("1232");
